ff863b8aa5784c869895d53503188965c154809a
OriginKim <gohouse@gmail.com>
2025-03-06 00:18:15 +0900

    First commit : Add Kpop_GAME project

---
+++ Kpop_GAME.py
@@ -0,0 +1,152 @@
+import winsound
+import time
+import random
+
+
+songs = [
+    "Baddie1.wav",
+    "Bubble1.wav",
+    "Chasing That Feeling1.wav",
+    "Cool With You1.wav",
+    "Either Way1.wav",
+    "ETA1.wav",
+    "Get A Guitar1.wav",
+    "Hypeboy1.wav",
+    "Seven1.wav",
+    "Smoke1.wav",
+    "Solo1.wav",
+    "Vancouver21.wav",
+    "You&Me1.wav",
+    "Underwater1.wav",
+    "Fast Forward1.wav",
+    "Trip1.wav",
+    "Off The Record1.wav",
+    "Shut Down1.wav",
+    "I love my body1.wav"
+   
+    # 추가 노래 파일 업뎃
+]
+
+answers = [
+    "Baddie",
+    "Bubble",
+    "Chasing That Feeling",
+    "Cool With You",
+    "Either Way",
+    "ETA",
+    "Get A Guitar",
+    "Hype boy",
+    "Seven",
+    "Smoke",
+    "Solo",
+    "Vancouver2",
+    "You&Me",
+    "Underwater",
+    "Fast Forward",
+    "Trip",
+    "Off The Record",
+    "Shut Down",
+    "I love my body"
+
+    # 노래정답 업뎃
+]
+
+sound_effect = [
+    "correct.wav",
+    "wrong.wav"
+]
+
+input("-------------노래맞추기 게임-------------\n\n     노래가 끝날때까지 기다렸다가\n정답칸이 나오면 그때 정답을 입력하세요!\n     아무 키나 눌러 시작하세요!!")
+
+def main():
+    correct_answers = 0
+
+   
+
+    # 랜덤플레이
+    song_indices = list(range(len(songs)))
+    random.shuffle(song_indices)
+
+    for i in range(5):
+        song_index = song_indices[i]
+        song_file = songs[song_index]
+
+        print(f"\n문제 {i + 1}")
+
+        # 첫 번째 기회: 1초 노래 재생
+        winsound.PlaySound(song_file, winsound.SND_FILENAME)
+        
+        enter_answer = input("첫 번째 기회: 정답을 입력하세요 (1번째 기회): ").lower()
+
+        if enter_answer == answers[song_index].lower():
+            print("정답입니다!")
+            winsound.PlaySound(sound_effect[0], winsound.SND_FILENAME)
+            correct_answers += 1
+            continue
+
+        # 두 번째 기회: 3초 노래 재생
+        print("틀렸습니다!")
+        winsound.PlaySound(sound_effect[1], winsound.SND_FILENAME)
+        song_file = songs[song_index][:-5] + '3.wav'
+        winsound.PlaySound(song_file, winsound.SND_FILENAME)
+       
+        enter_answer = input("두 번째 기회: 정답을 다시 입력하세요 (2번째 기회): ").lower()
+
+        if enter_answer == answers[song_index].lower():
+            print("정답입니다! 다음 문제로 넘어갑니다.")
+            winsound.PlaySound(sound_effect[0], winsound.SND_FILENAME)
+            correct_answers += 1
+            continue
+
+        # 세 번째 기회: 5초 노래 재생
+        print("틀렸습니다!")
+        winsound.PlaySound(sound_effect[1], winsound.SND_FILENAME)
+        song_file = songs[song_index][:-5] + '5.wav'
+        winsound.PlaySound(song_file, winsound.SND_FILENAME)
+       
+        enter_answer = input("세 번째 기회: 정답을 다시 입력하세요 (3번째 기회): ").lower()
+
+        if enter_answer == answers[song_index].lower():
+            print("정답입니다!")
+            winsound.PlaySound(sound_effect[0], winsound.SND_FILENAME)
+            correct_answers += 1
+        else:
+            print(f"정답은 \"{answers[song_index]}\"입니다.")
+            winsound.PlaySound(sound_effect[1], winsound.SND_FILENAME)
+
+
+    print(f"\n게임 종료\n맞춘 문제 수: {correct_answers} / 5")
+
+    play_again = input("\n\n-게임을 다시 하려면 1번을 누르고 엔터를 쳐주세요!\n-종료하려면 엔터를 쳐주세요! ").lower()
+
+    if play_again == "1":
+        # 게임을 다시 시작
+        main()
+    else:
+        print('''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⡴⠒⠚⣻⠇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠓⠒⠒⠒⠒⢤⣤⠴⠚⠉⠀⡸⠁⣠⠞⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⣠⠖⠋⠀⠀⠀⠀⢀⡧⠞⠣⠤⣀⡀⠀⠀⠀⠀
+⢀⣤⠔⠒⠚⣏⠉⠉⠉⠉⠉⠉⠉⠒⠒⠲⠤⠒⠋⠉⠉⠉⠉⠉⠒⠒⠻⢴⠋⠀⠀⠀⠀⠀⣠⠔⠋⠀⠀⠀⠀⠀⠉⠑⠲⢤⡀ 다음에 또 와!
+⠈⠙⠒⠤⢄⣘⣦⡀⠀⠀⠀⠀⠀⠀⡔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠤⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼
+⠀⠀⠀⠀⠀⠀⠈⢉⣿⣗⡒⠒⠒⡾⠁⣠⣶⠒⡆⠀⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠞⠀
+⠀⠀⠀⠀⠀⠀⢠⡎⠀⠀⠙⢦⣀⠇⠀⠻⣼⡿⠁⠀⠀⢠⡄⠀⠀⠸⣷⣼⣷⠀⢸⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⣏⠀⠀⠀⠀⡿⠖⠲⣄⠀⠀⣤⡀⢀⣤⣀⠀⠀⢀⠈⠋⠁⠀⢸⣿⡉⠓⠦⣀⡀⠀⠀⠀⠀⢀⡴⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢹⡀⠀⠀⠀⡇⠀⠀⣸⠀⠀⢸⣯⠟⠛⠛⢿⣿⠋⠀⢰⠟⠉⠹⡇⢷⠀⠀⠀⠉⠓⠦⣄⣠⠎⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⠹⡦⠴⠋⠀⠀⠀⢹⡄⠀⢀⡼⠁⠀⠀⣇⠀⠀⢠⡇⣀⣧⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⡄⠀⠀⠀⠙⢆⠀⠀⠀⠀⠀⠹⠤⠋⠀⠀⠀⠀⠈⠓⡶⠋⠙⠳⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⠀⠀⠀⠀⠑⠶⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠖⠋⠀⠀⠀⠀⠀⠀⠉⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣶⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⠀⠀⠀⠀⠀⠀⢀⣷⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠤⣤⠤⠴⠒⠒⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⡆⢀⣠⠤⠒⠒⠒⠂⠀⠀⠐⠒⠒⠒⠒⠲⢦⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⡟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠒⠾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀''')
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
